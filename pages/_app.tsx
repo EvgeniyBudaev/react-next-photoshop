e9098e5248ebcc6f03a40040866f7802e6c4ecda
Evgeniy Budaev <budaev.e@gmail.com>
2021-06-19 14:29:13 +0300

    Шрифты и цвета

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import React from 'react';
 import { AppProps } from 'next/dist/next-server/lib/router/router';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>Photoshop курсы</title>
+      <link rel="stylesheet" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap" rel="stylesheet" />
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
 
 export default MyApp;
