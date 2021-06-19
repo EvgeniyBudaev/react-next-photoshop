f6ebb01c64155ac2c42516e36a30d7cd7cb05bd7
Evgeniy Budaev <budaev.e@gmail.com>
2021-06-19 18:54:01 +0300

    Компонент p

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Htag } from '../components';
+import { Button, Htag, Ptag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
@@ -7,6 +7,9 @@ export default function Home(): JSX.Element {
       <Htag tag='h1'>text</Htag>
       <Button appearance='primary' arrow='right'>Кнопка</Button>
       <Button appearance='ghost' arrow='down'>Кнопка</Button>
+      <Ptag size="l">Большой</Ptag>
+      <Ptag>Средний</Ptag>
+      <Ptag size="s">Маленький</Ptag>
     </>
   );
 }
