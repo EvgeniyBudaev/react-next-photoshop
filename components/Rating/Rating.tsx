8aa65d1976a78b49465e454e162a5ee46e4563d7
Evgeniy Budaev <budaev.e@gmail.com>
2021-06-20 07:09:16 +0300

    Компонент рейтинга - 2

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, KeyboardEvent } from 'react';
 import cn from 'classnames';
 import StarIcon from './Star.svg';
 import { RatingProps } from './Rating.props';
@@ -15,15 +15,39 @@ export const Rating = ({ isEditable = false, rating, setRating, ...props }: Rati
 	const constructRating = (currentRating: number) => {
 		const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
 			return (
-				<StarIcon
+				<span
 					className={cn(styles.star, {
-						[styles.filled]: i < currentRating
+						[styles.filled]: i < currentRating,
+						[styles.editable]: isEditable
 					})}
-				/>
+					onMouseEnter={() => changeDisplay(i + 1)}
+					onMouseLeave={() => changeDisplay(rating)}
+					onClick={() => onClick(i + 1)}
+				>
+					<StarIcon
+						tabIndex={isEditable ? 0 : -1}
+						onKeyDown={(e: KeyboardEvent<SVGAElement>) => isEditable && handleSpace(i + 1, e)}
+					/>
+				</span>
 			);
 		});
 
 		setRatingArray(updatedArray);
+	};
+
+	const changeDisplay = (i: number) => {
+		if (!isEditable) return;
+		constructRating(i);
+	};
+
+	const onClick = (i: number) => {
+		if (!isEditable || !setRating) return;
+		setRating(i);
+	};
+
+	const handleSpace = (i: number, e: KeyboardEvent<SVGAElement>) => {
+		if (e.code != 'Space' || !setRating) return;
+		setRating(i);
 	};
 
 	return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Htag, Ptag, Rating, Tag } from '../components';
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(4);
+
   return (
     <>
       <Htag tag='h1'>text</Htag>
@@ -13,7 +15,7 @@ export default function Home(): JSX.Element {
       <Tag size='s' color='green'>Small</Tag>
       <Tag size='m' color='red'>Medium</Tag>
       <Tag color='primary'>Primary</Tag>
-      <Rating rating={4} />
+      <Rating rating={rating} isEditable setRating={setRating} />
     </>
   );
 }
