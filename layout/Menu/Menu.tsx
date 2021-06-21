5b8f9f1113a7f69a28846dec533651f2d7065b7e
Evgeniy Budaev <budaev.e@gmail.com>
2021-06-21 05:29:57 +0300

    Структура роутинга

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -4,20 +4,8 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { AppContext } from '../../context/app.context';
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
-import CoursesIcon from './icons/Courses.svg';
-import ServicesIcon from './icons/Services.svg';
-import BooksIcon from './icons/Books.svg';
-import ProductsIcon from './icons/Products.svg';
-import { TopLevelCategory } from '../../interfaces/page.interface';
+import { firstLevelMenu } from '../../helpers/helpers';
 import styles from './Menu.module.css';
-
-
-const firstLevelMenu: FirstLevelMenuItem[] = [
-	{ route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
-	{ route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
-	{ route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
-	{ route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products }
-];
 
 export const Menu = (): JSX.Element => {
 	const { menu, setMenu, firstCategory } = useContext(AppContext);
