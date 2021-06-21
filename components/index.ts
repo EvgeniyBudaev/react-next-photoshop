d39cf12f7574fabf14165366904d82825781245f
Evgeniy Budaev <budaev.e@gmail.com>
2021-06-21 07:41:30 +0300

    Вёрстка страницы продуктов - 1

--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,6 @@ export * from './Button/Button';
 export * from './Htag/Htag';
 export * from './Ptag/Ptag';
 export * from './Rating/Rating';
+export * from './Card/Card';
+export * from './HhData/HhData';
 export * from './Tag/Tag';
